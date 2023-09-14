d32c4a20e2336959956153aa31935ba1e7d907c2
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-14 19:54:52 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje8/InicijalnoPonavljanje8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi broj n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] niz = new int[n+1];
+            niz[0] = 1;
+            niz[1] = 2;
+            for(int i = 2; i < (n-1); i++)
+            {
+                niz[i] = niz[i - 1] + niz[i - 2];
+                //Console.WriteLine(niz[i]);
+            }
+            Console.WriteLine(n+" član Fibonaccievog niza je: " + niz[n-2]);
+            Console.ReadKey();
+        }
+    }
+}
